5d31cf18b3da623b6ac481007e71c7360970df3a
yana <neauhades@126.com>
2016-03-08 21:31:13 +0800

    '20160308'

---
+++ c++/631A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int a[1005];
+int main()
+{
+	int n, l, r;
+	scanf("%d", &n);
+	int ans = 0;
+	for(int i = 0; i < n-1; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	return 0;
+}

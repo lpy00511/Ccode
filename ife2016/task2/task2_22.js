b8483ae02e2a28f9596c40a1df1d7e2d88dea35b
lpy00511 <neauhades@126.com>
2017-06-02 22:47:30 +0800

    li

--- ife2016/task2/task2_22.js
+++ ife2016/task2/task2_22.js
@@ -8,7 +8,7 @@ function addEventHandler(element,type,handler){
   }
 }
 function getInputNode(){
-	var btn = document.getElementById("insert");
+	var btn = document.getElementsByName("insert");
 	addEventHandler(btn,'click',function(){
 		var nodeArr = document.getElementById("input-node").value;
 		console.log(nodeArr);

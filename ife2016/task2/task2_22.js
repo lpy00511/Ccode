6b19bc245bae57151ae28fc32d6bd961e65f2588
lpy00511 <neauhades@126.com>
2017-06-02 18:53:28 +0800

    li

---
+++ ife2016/task2/task2_22.js
@@ -0,0 +1,20 @@
+function addEventHandler(element,type,handler){
+  if(element.addEventListener){
+    element.addEventListener(type,handler,false);
+  }else if(element.attachEvent){
+    element.attachEvent("on"+type,handler);
+  }else {
+    element["on"+type] = handler;
+  }
+}
+function getInputNode(){
+	var btn = document.getElementById("insert");
+	addEventHandler(btn,'click',function(){
+		var nodeArr = document.getElementById("input-node").value;
+		console.log(nodeArr);
+	});
+}
+function init(){
+	getInputNode();
+}
+init();

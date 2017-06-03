fd71ba9088b364477eeb4f1e4b4789c3081772a5
lpy00511 <neauhades@126.com>
2017-06-03 20:51:35 +0800

    'li'

---
+++ ife2016/task2/task2_22_1.js
@@ -0,0 +1,9 @@
+function addEventHandler(element,type,handler){
+  if(element.addEventListener){
+    element.addEventListener(type,handler,false);
+  }else if(element.attachEvent){
+    element.attachEvent("on"+type,handler);
+  }else {
+    element["on"+type] = handler;
+  }
+}

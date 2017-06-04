25f64ee05957eb2bc48f02762657701ed69dfbf0
lpy00511 <neauhades@126.com>
2017-06-04 20:42:16 +0800

    'W'

--- ife2016/task2/task2_22_1.js
+++ ife2016/task2/task2_22_1.js
@@ -7,3 +7,13 @@ function addEventHandler(element,type,handler){
     element["on"+type] = handler;
   }
 }
+function draw(){
+	var tree = document.getElementById("chart-warp");
+	var node-top = document.createElement("div");
+	node_top.setAttribute("class", "circle");
+	tree.appendChild("node_top");
+	var line-top-left = document.createElement("div");
+}
+ window.onload = function(){
+ 	draw();
+ }

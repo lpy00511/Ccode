4b150d52878d89dfd1f5772c41ab0a2684e04961
lpy00511 <neauhades@126.com>
2017-06-05 17:06:18 +0800

    M

--- ife2016/task2/task2_22_1.js
+++ ife2016/task2/task2_22_1.js
@@ -7,6 +7,8 @@ function addEventHandler(element,type,handler){
     element["on"+type] = handler;
   }
 }
+var nodeArr = [11,7,15,5,9,13,20,3,8,10,12,14,25];
+var node=[];
 function draw(){
 	var tree = document.getElementById("chart-wrap");
 	var node_top = document.createElement("div");
@@ -18,24 +20,30 @@ function draw(){
 	var right_line = document.createElement("div");
 	right_line.className="right-line";
 	tree.appendChild(right_line);
+	var index_left = window.getComputedStyle(node_top, "").left.indexOf('px');
+	var index_top = window.getComputedStyle(node_top,"").top.indexOf('px');
+	
 	var node1 = document.createElement("div");
 	node1.className="circle";
 	tree.appendChild(node1);
-	node1.style.left= Number(window.getComputedStyle(node_top, "").left.substring(0, 3))-150+'px';
-	node1.style.top = Number(window.getComputedStyle(node_top, "").top.substring(0, 2))+100+'px';
+	node1.style.left= Number(window.getComputedStyle(node_top, "").left.substring(0, index_left))-150+'px';
+	node1.style.top = Number(window.getComputedStyle(node_top, "").left.substring(0, index_top))+100+'px';
 	var node2 = document.createElement("div");
 	node2.className="circle";
 	tree.appendChild(node2);
-	node2.style.left= Number(window.getComputedStyle(node_top, "").left.substring(0, 3))+150+'px';
-	node2.style.top = Number(window.getComputedStyle(node_top, "").top.substring(0, 2))+100+'px';
-	// var node_top = '';
-	// node_top += '<div class="circle"></div>';
-	// node_top += '<div class="left-line"></div>';
-	// node_top += '<div class="right-line"></div>';
-
-	// tree.innerHTML =node_top;
+	console.log(index_top);
+	node2.style.left= Number(window.getComputedStyle(node_top, "").left.substring(0, index_left))+150+'px';
+	node2.style.top = Number(window.getComputedStyle(node_top, "").top.substring(0, index_top))+100+'px';
 
 }
  window.onload = function(){
  	draw();
  }
+
+
+
+
+
+
+
+
